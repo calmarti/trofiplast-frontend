a63158639c15bc66f3ffbb7310d2630207bd988b
Ivan Martinez <ivmartinezc@gmail.com>
2022-06-25 14:57:19 +0200

    componente Search: 3 Rows de 2 Col con 6 primeros filtros

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,7 +2,7 @@ import Sidebar from "../layout/Sidebar";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Col, { useCol } from "react-bootstrap/Col";
 import Stack from "react-bootstrap/Stack";
 import Alert from "react-bootstrap/Alert";
 
@@ -14,53 +14,115 @@ export default function Search() {
           <Sidebar />
         </Col>
 
-
         <Col className="px-0">
           <Alert className="search-alert-component py-5" variant="primary">
-          <Alert.Heading  className="search-alert-heading">
-          Search the Cronoplast database
-          </Alert.Heading>
-          <hr/>
-          <p className="search-alert-summary">Cronoplast contains 300 registers of plastic ingestion by animals that spans 120 years in time across several geographical areas worldwide. Its sources are mainly scientific literature but it include other type of references.</p>
-           <hr/>
-            <Form>              
+            <Alert.Heading className="search-alert-heading">
+              Search the Cronoplast database
+            </Alert.Heading>
+            <hr />
+            <p className="search-alert-summary">
+              Cronoplast contains 300 registers of plastic ingestion by animals
+              that spans 120 years in time across several geographical areas
+              worldwide. Its sources are mainly scientific literature but it
+              include other type of references.
+            </p>
+            <hr />
+            <br/>
+            <Form>
+              
               <Container className="w-50">
+                {/* <Stack className="my-5 py-4 px-5 bg-light border" gap={1}> */}
+                {/*    <Form.Text>Grupo taxonómico</Form.Text> */}
+              
                 <Row>
-              {/*   wrapper con borde */}
-              <Stack className="my-5 py-4 bg-light border" gap={1}>
-                <Form.Text>Grupo taxonómico</Form.Text>
-                <Form.Select className="mx-5 my-2 w-75">
-                  <option>General group</option>
-                  <option value="1">Fish</option>
-                  <option value="2">Terrestrial Mammal</option>
-                  <option value="3">Three</option>
-                </Form.Select>
+                <Form.Group as={Col} className="mx-4">
+                  <Form.Label>
+                     <span className="select-label">General group</span>
+                    <Form.Select className="mx-5 my-3" defaultValue="">
+                      <option></option>
+                      <option value="1">Fish</option>
+                      <option value="2">Terrestrial Mammal</option>
+                      <option value="3">Three</option>
+                    </Form.Select>
+                  </Form.Label>
+                </Form.Group>
 
-                <Form.Select className="mx-5 my-2 w-75">
-                  <option>Family</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </Form.Select>
+                <Form.Group as={Col} className="mx-4">
+                  <Form.Label>
+                  <span className="select-label">Family</span>
+                    <Form.Select className="mx-5 my-3" defaultValue="">
+                      <option></option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">A long option indeed</option>
+                    </Form.Select>
+                  </Form.Label>
+                </Form.Group>
+                </Row>
+                </Container>
 
-                <Form.Select className="mx-5 my-2 w-75">
-                  <option>Genus</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </Form.Select>
-                <Form.Select className="mx-5 my-2 w-75">
-                  <option>Species</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </Form.Select>
-              </Stack>
-              {/*  wrapper con borde  */}
-              </Row>
+            <Container className="w-50">
+                <Row>
+                <Form.Group as={Col} className="mx-4">
+                  <Form.Label>
+                  <span className="select-label">Genus</span>
+                    <Form.Select className="mx-5 my-3" defaultValue="">
+                      <option></option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">A long option indeed</option>
+                    </Form.Select>
+                  </Form.Label>
+                </Form.Group>
+
+                <Form.Group as={Col} className="mx-4">
+                  <Form.Label>
+                    <span className="select-label">Species</span>
+                    <Form.Select className="mx-5 my-3" defaultValue="">
+                      <option></option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">A long option indeed</option>
+                    </Form.Select>
+                  </Form.Label>
+                </Form.Group>
+               </Row>
               </Container>
 
+              <Container className="w-50">
+              <Row>
+              
+              <Form.Group as={Col} className="mx-4">
+                  <Form.Label>
+                  <span className="select-label">Area</span>
+                    <Form.Select className="mx-5 my-3" defaultValue="">
+                      <option></option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">A long option indeed</option>
+                    </Form.Select>
+                  </Form.Label>
+                </Form.Group>
 
+                <Form.Group as={Col} className="mx-4">
+                  <Form.Label>
+                  <span className="select-label">Country</span>
+                    <Form.Select className="mx-5 my-3" defaultValue="">
+                      <option></option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">A long option indeed</option>
+                    </Form.Select>
+                  </Form.Label>
+                </Form.Group>
+
+                </Row>
+                </Container>
+
+                {/* componente Calendar que recibe From y To  */}
+                {/* botón de reset y botón de continue */}
+
+                {/*   </Stack> */}
             </Form>
           </Alert>
         </Col>
