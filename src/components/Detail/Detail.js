1819f46c1cd5f6a29707a8b9c9d00e2c2e7d5d50
Ivan Martinez <ivmartinezc@gmail.com>
2022-07-01 20:08:21 +0200

    Detail: llamada al api con useEffect y cambio en el state

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -4,10 +4,19 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Sidebar from "../layout/Sidebar";
 import Alert from "react-bootstrap/Alert";
+import { useState, useEffect } from "react";
+import client from "../../api/client";
 
 export default function Detail() {
+  const [item, setItem] = useState(null);
   const params = useParams();
   const id = params.id;
+
+  useEffect(() => {
+    client.get(`/apiv1/items/${id}`).then((item) => setItem(item));
+  }, [id]);
+
+  console.log('item', item);
 
   return (
     <Container fluid>
